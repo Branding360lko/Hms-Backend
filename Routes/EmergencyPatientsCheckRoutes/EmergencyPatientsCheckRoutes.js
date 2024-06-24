f3dec9e85d8522e34077d87a8bb247fa9decfa79
Arman Ali <armanal3066@gmail.com>
2024-06-24 11:18:57 +0530

    Emergency Patients get medicine data,lab test data and doctor visit charges and get emergency patient total of medicine on daily basic,Lab test total daily basic and doctor daily basic charges

--- Routes/EmergencyPatientsCheckRoutes/EmergencyPatientsCheckRoutes.js
+++ Routes/EmergencyPatientsCheckRoutes/EmergencyPatientsCheckRoutes.js
@@ -227,4 +227,150 @@ Router.post("/update-EmergencyPatientsChecked/:Id", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+Router.get("/get-one-emergency-patients-data/:Id", async (req, res) => {
+  const Id = req.params.Id;
+  try {
+    const EmergencyPatientData = await EmergencyPatientsCheck.aggregate([
+      {
+        $match: {
+          mainId: Id,
+        },
+      },
+      {
+        $lookup: {
+          from: "emergencypatients",
+          localField: "mainId",
+          foreignField: "mainId",
+          as: "EmergencyPatientData",
+        },
+      },
+      {
+        $unwind: "$EmergencyPatientData",
+      },
+      {
+        $lookup: {
+          from: "patients",
+          localField: "EmergencyPatientData.patientId",
+          foreignField: "patientId",
+          as: "patientsData",
+        },
+      },
+      {
+        $lookup: {
+          from: "doctors",
+          localField: "doctorId",
+          foreignField: "_id",
+          as: "doctorData",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          Symptoms: 1,
+          Note: 1,
+          VisitDateTime: 1,
+          isPatientsChecked: 1,
+          medicine: 1,
+          test: 1,
+          IpdPatientData: 1,
+          patientsData: 1,
+          doctorData: 1,
+          ReferedDoctor: 1,
+          submittedBy: 1,
+        },
+      },
+    ]);
+    if (!EmergencyPatientData) {
+      return res.status(403).json({ message: "No Patients Data Find " });
+    }
+    return res
+      .status(200)
+      .json({ message: "Successfully Data Fetch", data: EmergencyPatientData });
+  } catch (error) {
+    res.status(500).json("internal server error");
+  }
+});
+Router.get("/get-one-emergency-patients-data-total/:Id", async (req, res) => {
+  const Id = req.params.Id;
+  try {
+    const EmergencyPatientData = await EmergencyPatientsCheck.aggregate([
+      {
+        $match: {
+          mainId: Id,
+        },
+      },
+      {
+        $lookup: {
+          from: "doctors",
+          localField: "doctorId",
+          foreignField: "_id",
+          as: "doctorData",
+        },
+      },
+      {
+        $lookup: {
+          from: "doctorprofessionaldetails",
+          localField: "doctorData.doctorId",
+          foreignField: "doctorId",
+          as: "doctorFeesDatails",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          VisitDateTime: 1,
+          doctorData: 1,
+          doctorFeesDatails: "$doctorFeesDatails.doctorFee",
+          DailyMedicinePriceTotal: { $sum: "$medicine.Price" },
+          DailyTestPriceTotal: { $sum: "$test.Price" },
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          DailyMedicinePriceTotal: { $first: "$DailyMedicinePriceTotal" },
+          DailyTestPriceTotal: { $first: "$DailyTestPriceTotal" },
+          visitDate: { $first: "$VisitDateTime" },
+          doctorFeesDatails: { $first: "$doctorFeesDatails" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          DailyMedicinePriceTotal: 1,
+          DailyTestPriceTotal: 1,
+          visitDate: 1,
+          doctorFeesDatails: 1,
+          doctorVisitCharge: 1,
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          overAllData: { $push: "$$ROOT" },
+          overallTotalMedicinePrice: { $sum: "$DailyMedicinePriceTotal" },
+          overallTotalTestPrice: { $sum: "$DailyTestPriceTotal" },
+          overallDoctorVisitCharge: { $sum: "$doctorVisitCharge" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          overAllData: 1,
+          overallTotalMedicinePrice: 1,
+          overallTotalTestPrice: 1,
+          overallDoctorVisitCharge: 1,
+        },
+      },
+    ]);
+    if (!EmergencyPatientData) {
+      return res.status(403).json({ message: "No Data Found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Successfully Data Fetch", data: EmergencyPatientData });
+  } catch (error) {
+    res.status(500).json("internal server error");
+  }
+});
 module.exports = Router;
